3caebbf7d470bbbd57e8f47c02ba071090f3e48f
mauriciosierrac <2376@holbertonschool.com>
2021-08-09 16:06:03 -0500

    upload end files

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex : true
 })
 .then(db=>{
     console.log('DB Connect!')
